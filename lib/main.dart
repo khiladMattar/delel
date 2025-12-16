9bd7df75ecea9ff479f87b0e251c229024ef8f79
khiladMattar <kmtr467@gmail.com>
2025-12-16 15:45:00 +0200

    scand

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:daiel/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const daiel());
+  runApp(const Daiel());
 }
 
-class daiel extends StatelessWidget {
-  const daiel({super.key});
+class Daiel extends StatelessWidget {
+  const Daiel({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +18,7 @@ class daiel extends StatelessWidget {
         title: const Text("daiel"),
       ),
       body: Center(
-        child: Image.asset(Assets.assetsImagesOnBording2)
+        child: Image.asset(Assets.assetsImagesOnBording1)
       ),
     ));
   }
